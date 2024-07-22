1ad93ab6d7e00adb5d44d2f096038a2ffb048a98
Tarun Kumar <tarunnimbal123@gmail.com>
2024-07-22 22:37:31 +0530

    bullmq based infra setup and runnung

--- src/index.ts
+++ src/index.ts
@@ -1,10 +1,20 @@
-import express, {Express}from "express";
+import express, { Express } from "express";
 import serverConfig from "./config/serverConfig";
 import apiRouter from "./routes";
-const app : Express = express();
-
+import SampleWorker from "./workers/SampleWorker";
+import SampleQueueProducer from "./producers/SampleQueueProducer";
+const app: Express = express();
 
 app.use("/api", apiRouter);
-app.listen(serverConfig.PORT, ()=>{
-    console.log("Server is start at PORT :",3000);
+app.listen(serverConfig.PORT, () => {
+  console.log("Server is start at PORT :", 3000);
+
+  SampleWorker("SampleQueue");
+
+  SampleQueueProducer("SampleJob", {
+    name: "Tarun",
+    company: "Student",
+    position: "4th Year",
+    locatiion: "Nit Kurukshetra",
+  });
 });
